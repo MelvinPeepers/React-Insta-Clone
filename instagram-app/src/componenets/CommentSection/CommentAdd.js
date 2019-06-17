c2d24831f888eac323daf1fa8a3c9889bf607f0a
Melvin Peepers <peepersm@gmail.com>
2019-06-16 23:02:44 -0400

    Some tweaks to the CommentSectin CSS

--- instagram-app/src/componenets/CommentSection/CommentAdd.js
+++ instagram-app/src/componenets/CommentSection/CommentAdd.js
@@ -3,7 +3,7 @@ import React from "react";
 const CommentAdd = props => {
   return (
     <form>
-      <input type="text" plaecholder="Add comment..." />
+      <input type="text" plaecholder="Add a comment..." />
     </form>
   );
 };
